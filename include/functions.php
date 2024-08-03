7dcc66b48c31384a6fb2f6cbde467a8087e338d3
xmartina <martinsprince126@gmail.com>
2024-08-03 11:39:58 +0100

    init

--- include/functions.php
+++ include/functions.php
@@ -1,6 +1,9 @@
 <?php
 include_once ('config.php');
 //General Settings
+$sql = "SELECT * FROM general_settings"; // Adjust the table name if different
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
 
 $site_name = $row["site_name"];
 $site_dec = $row["site_dec"];
