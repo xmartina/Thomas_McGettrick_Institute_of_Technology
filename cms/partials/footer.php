71d554257172ca0b79d6db50a413486abcafbf5f
xmartina <martinsprince126@gmail.com>
2024-09-09 15:45:15 +0100

    d

--- cms/partials/footer.php
+++ cms/partials/footer.php
@@ -1,1 +1,42 @@
-<?php
+<!-- jQuery library js -->
+<script src="assets/js/lib/jquery-3.7.1.min.js"></script>
+<!-- Bootstrap js -->
+<script src="assets/js/lib/bootstrap.bundle.min.js"></script>
+<!-- Apex Chart js -->
+<script src="assets/js/lib/apexcharts.min.js"></script>
+<!-- Data Table js -->
+<script src="assets/js/lib/dataTables.min.js"></script>
+<!-- Iconify Font js -->
+<script src="assets/js/lib/iconify-icon.min.js"></script>
+<!-- jQuery UI js -->
+<script src="assets/js/lib/jquery-ui.min.js"></script>
+<!-- Vector Map js -->
+<script src="assets/js/lib/jquery-jvectormap-2.0.5.min.js"></script>
+<script src="assets/js/lib/jquery-jvectormap-world-mill-en.js"></script>
+<!-- Popup js -->
+<script src="assets/js/lib/magnifc-popup.min.js"></script>
+<!-- Slick Slider js -->
+<script src="assets/js/lib/slick.min.js"></script>
+<!-- main js -->
+<script src="assets/js/app.js"></script>
+
+<script>
+    // ================== Password Show Hide Js Start ==========
+    function initializePasswordToggle(toggleSelector) {
+        $(toggleSelector).on('click', function() {
+            $(this).toggleClass("ri-eye-off-line");
+            var input = $($(this).attr("data-toggle"));
+            if (input.attr("type") === "password") {
+                input.attr("type", "text");
+            } else {
+                input.attr("type", "password");
+            }
+        });
+    }
+    // Call the function
+    initializePasswordToggle('.toggle-password');
+    // ========================= Password Show Hide Js End ===========================
+</script>
+
+</body>
+</html>
