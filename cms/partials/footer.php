726e947eb271ab5ed660164aff0a93963fddc9ba
xmartina <martinsprince126@gmail.com>
2024-09-09 15:51:00 +0100

    d

--- cms/partials/footer.php
+++ cms/partials/footer.php
@@ -1,24 +1,24 @@
 <!-- jQuery library js -->
-<script src="assets/js/lib/jquery-3.7.1.min.js"></script>
+<script src="<?=$siteUrl?>assets/js/lib/jquery-3.7.1.min.js"></script>
 <!-- Bootstrap js -->
-<script src="assets/js/lib/bootstrap.bundle.min.js"></script>
+<script src="<?=$siteUrl?>assets/js/lib/bootstrap.bundle.min.js"></script>
 <!-- Apex Chart js -->
-<script src="assets/js/lib/apexcharts.min.js"></script>
+<script src="<?=$siteUrl?>assets/js/lib/apexcharts.min.js"></script>
 <!-- Data Table js -->
-<script src="assets/js/lib/dataTables.min.js"></script>
+<script src="<?=$siteUrl?>assets/js/lib/dataTables.min.js"></script>
 <!-- Iconify Font js -->
-<script src="assets/js/lib/iconify-icon.min.js"></script>
+<script src="<?=$siteUrl?>assets/js/lib/iconify-icon.min.js"></script>
 <!-- jQuery UI js -->
-<script src="assets/js/lib/jquery-ui.min.js"></script>
+<script src="<?=$siteUrl?>assets/js/lib/jquery-ui.min.js"></script>
 <!-- Vector Map js -->
-<script src="assets/js/lib/jquery-jvectormap-2.0.5.min.js"></script>
-<script src="assets/js/lib/jquery-jvectormap-world-mill-en.js"></script>
+<script src="<?=$siteUrl?>assets/js/lib/jquery-jvectormap-2.0.5.min.js"></script>
+<script src="<?=$siteUrl?>assets/js/lib/jquery-jvectormap-world-mill-en.js"></script>
 <!-- Popup js -->
-<script src="assets/js/lib/magnifc-popup.min.js"></script>
+<script src="<?=$siteUrl?>assets/js/lib/magnifc-popup.min.js"></script>
 <!-- Slick Slider js -->
-<script src="assets/js/lib/slick.min.js"></script>
+<script src="<?=$siteUrl?>assets/js/lib/slick.min.js"></script>
 <!-- main js -->
-<script src="assets/js/app.js"></script>
+<script src="<?=$siteUrl?>assets/js/app.js"></script>
 
 <script>
     // ================== Password Show Hide Js Start ==========
