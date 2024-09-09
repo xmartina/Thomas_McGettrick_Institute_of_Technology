7b4fa1f7808aded46d12707da12d8bd2edee545a
xmartina <martinsprince126@gmail.com>
2024-09-09 17:10:35 +0100

    d

--- cms/functions/main_function.php
+++ cms/functions/main_function.php
@@ -1,15 +1,5 @@
 <?php
 
-// Get the current URL
-$currentUrl = $_SERVER['REQUEST_URI'];
-
-// Check if the URL contains 'cms'
-if (strpos($currentUrl, 'cms') !== false) {
-    $rootDir = '/home/multistream6/domains/thomas.matagram.com/public_html/cms/';
-} else {
-    $rootDir = '/home/multistream6/domains/thomas.matagram.com/public_html/';
-}
-
 include_once($rootDir.'include/config.php');
 // General Settings
 $sql = "SELECT * FROM general_settings"; // Adjust the table name if different
