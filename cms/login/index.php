e112c6ddd75639e01693970d01ee4da3878ad6d5
xmartina <martinsprince126@gmail.com>
2024-09-09 15:46:45 +0100

    d

--- cms/login/index.php
+++ cms/login/index.php
@@ -5,14 +5,14 @@ include_once ($rootDir.'partials/header.php');
 <section class="auth bg-base d-flex flex-wrap">
     <div class="auth-left d-lg-block d-none">
         <div class="d-flex align-items-center flex-column h-100 justify-content-center">
-            <img src="assets/images/auth/auth-img.png" alt="">
+            <img src="<?=$rootDir?>assets/images/auth/auth-img.png" alt="">
         </div>
     </div>
     <div class="auth-right py-32 px-24 d-flex flex-column justify-content-center">
         <div class="max-w-464-px mx-auto w-100">
             <div>
                 <a href="index.html" class="mb-40 max-w-290-px">
-                    <img src="assets/images/logo.png" alt="">
+                    <img src="<?=$rootDir?>assets/images/logo.png" alt="">
                 </a>
                 <h4 class="mb-12">Sign In to your Account</h4>
                 <p class="mb-32 text-secondary-light text-lg">Welcome back! please enter your detail</p>
