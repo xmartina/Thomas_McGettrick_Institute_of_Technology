c033bc2e210fe957b813c6ede98878bb89fa4269
xmartina <martinsprince126@gmail.com>
2024-09-09 18:06:34 +0100

    d

--- cms/login/index.php
+++ cms/login/index.php
@@ -2,7 +2,6 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-session_start();
 $page_name = 'Login';
 $rootDir = '/home/multistream6/domains/thomas.matagram.com/public_html/';
 $siteUrl = 'https://thomas.matagram.com/';
